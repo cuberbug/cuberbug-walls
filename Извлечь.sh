951a97d2dfc270b98ca2d543842b5242017f890b
cuberbug <gigobug@gmail.com>
2024-10-19 00:30:06 +0300

    Auto: 2024-10-18 21:30:06

---
+++ Извлечь.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+# Определение текущей директории
+CURRENT_DIR=$(pwd)
+
+# Попытка найти эмулятор терминала
+TERMINAL="${TERMINAL:-$(command -v alacritty || command -v konsole || command -v gnome-terminal || command -v xterm)}"
+
+# Проверка, найден ли эмулятор
+if [ -z "$TERMINAL" ]; then
+    echo -e "\e[1;94m::\e[0m \e[31mОшибка:\e[0m Не удалось найти эмулятор терминала."
+    exit 1
+fi
+
+# Выводим найденный терминал
+echo -e "\e[1;94m::\e[0m \e[1mДля запуска используется терминал:\e[0m $TERMINAL"
+$TERMINAL -e $CURRENT_DIR/scripts/pull.sh
