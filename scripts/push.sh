ec5f2a776d9cb4eb942e7ec25e0a6dbbe5e1d9c7
cuberbug <gigobug@gmail.com>
2024-10-20 03:04:22 +0300

    Auto: 2024-10-20 00:04:22

--- scripts/push.sh
+++ scripts/push.sh
@@ -6,25 +6,21 @@ REPO_ROOT="$(git rev-parse --show-toplevel)"
 # Перейти в корневую директорию
 cd "$REPO_ROOT" || exit 1
 
-# Сохраняет дату и время в удобном формате
-DATE_TIME=$(date -u +"%Y-%m-%d %H:%M:%S")
-
-# Выводит результат
-echo -e "\e[1mТекущая дата и время (UTC):\e[0m $DATE_TIME"
-
 
 ## Взаимодействие с Git
 if git diff --quiet && git diff --cached --quiet; then
     echo -e "\e[1;94m::\e[0m \e[33mНет изменений для коммита.\e[0m"
 else
+    DATE_TIME=$(date -u +"%Y-%m-%d %H:%M:%S")
     echo -e "\e[1;94m::\e[0m Создание коммита"
     git add .
     git commit -m "Auto: $DATE_TIME"
 fi
+
 echo -e "\e[1;94m::\e[0m Сохранение и отправка изменений"
 if ! git push "$@"; then
     echo -e "\e[31mОшибка:\e[0m Не удалось выполнить push."
-    exit 1
+    exec bash
 fi
 
 
