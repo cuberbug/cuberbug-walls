db2976e7965d5ffed628d68c31b8827a47cca4f2
cuberbug <gigobug@gmail.com>
2024-10-20 04:55:19 +0300

    Auto: 2024-10-20 01:55:19

--- Сохранить.sh
+++ Сохранить.sh
@@ -3,6 +3,12 @@
 # Подключает конфигурацию с проверками и переменными
 source "$(dirname "$0")/scripts/config.sh"
 
+if [[ $# -eq 0 ]]; then
+    echo -e "\e[1;94m::\e[0m Поддерживаются аргументы: $0 [опции для git push]"
+else
+    echo -e "\e[1;94m::\e[0m git push выполнится со следующими опциями: $0"
+fi
+
 # Выводит найденный терминал и запускает в нём скрипт
 echo -e "\e[1;94m::\e[0m \e[1mДля запуска используется терминал:\e[0m $TERMINAL"
-$TERMINAL -e "$REPO_ROOT/scripts/push.sh"
+$TERMINAL -e "$REPO_ROOT/scripts/push.sh" "${@}"
