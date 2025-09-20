15b0b5d2cf59c08f41739d5db4d4510fb3c611f0
cuberbug <gigobug@gmail.com>
2025-09-20 22:10:37 +0300

    Auto: 2025-09-20 19:10:37

--- scripts/decor.sh
+++ scripts/decor.sh
@@ -3,15 +3,16 @@
 # Хранит константы для оформления и работы с текстом
 
 
-BOLD_DARK_RED="\e[1;31m"
-BOLD_DARK_BLUE="\e[1;94m"
-BOLD_GREEN="\e[1;92m"
-
-D_GREEN="\e[92m"
+D_GREEN="\e[32m"
 D_ORANGE="\e[33m"
 D_BOLD="\e[1m"
 D_CANCEL="\e[0m"
 
-BLUE_DECOR="${BOLD_DARK_BLUE}::${D_CANCEL}"
-ERROR_DECOR="${BOLD_DARK_RED}Ошибка:${D_CANCEL}"
-COMPLETE_DECOR="${BOLD_GREEN}Готово!${D_CANCEL}"
+BOLD_RED="\e[1;31m"
+BOLD_BLUE="\e[1;34m"
+BOLD_GREEN="\e[1;32m"
+
+
+BLUE_DECOR="${BOLD_BLUE}::${D_CANCEL}"
+ERROR_DECOR="${BOLD_RED}Ошибка:${D_CANCEL}"
+COMPLETE_DECOR="${BOLD_GREEN}Готово! ${D_CANCEL}"
