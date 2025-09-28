8eb9330f4d3225a9e400a5877639f2c193b05517
cuberbug <gigobug@gmail.com>
2025-09-29 00:20:56 +0300

    Auto: 2025-09-28 21:20:56

--- tools/git-manager/common.sh
+++ tools/git-manager/common.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# Устанавливаем "строгий режим" для безопасности
-# exit on error, exit on unset variable, fail pipe, prevent globbing over files
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -12,20 +10,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # --- Подключение зависимостей ---
 
-# Подключаем декор и утилиты, с проверкой на существование файлов
+# Подключает декор и утилиты, с проверкой на существование файлов
 source "${SCRIPT_DIR}/decor.sh" || { echo "Ошибка: не найден decor.sh" >&2; exit 1; }
 source "${SCRIPT_DIR}/utils.sh" || { echo "Ошибка: не найден utils.sh" >&2; exit 1; }
 
 
 # --- Проверки окружения ---
 
-# 1. Проверяем, что Git установлен
+# Проверяет, что Git установлен
 if ! command -v git &>/dev/null; then
     echo -e "${DECOR_ERROR} Git не установлен." >&2
     exit 1
 fi
 
-# 2. Проверяем, что мы находимся внутри Git-репозитория
+# Проверяет, что находится внутри Git-репозитория
 if ! git rev-parse --is-inside-work-tree &>/dev/null; then
     echo -e "${DECOR_ERROR} Не найден Git-репозиторий." >&2
     exit 1
@@ -34,10 +32,10 @@ fi
 
 # --- Глобальные переменные ---
 
-# Определяем корневую директорию репозитория
+# Определяет корневую директорию репозитория
 # Эта переменная будет доступна во всех скриптах, которые подключат common.sh
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
-# Экспортируем переменные, чтобы они были доступны в дочерних процессах, если понадобится
+# Экспорт переменных для доступа в дочерних процессах, если понадобится
 export REPO_ROOT
 export TERMINAL
---
+++ tools/git-manager/utils.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Запрашивает подтверждение выполнения действий, обёрнутых этой функцией.
+# Принимает строку, которая является запросом на подтверждение.
+confirm() {
+    local message=$1
+    while true; do
+        read -p "$(echo -e "${DECOR_BLUE} ${BOLD}${message}?${RESET} [Y/n]: ")" response
+        case "$response" in
+            [Yy]* | "") return 0 ;;
+            [Nn]* ) return 1 ;;
+            * ) echo -e "${DECOR_BLUE} ${FG_YELLOW}Неверный ввод, попробуйте снова.${RESET}" ;;
+        esac
+    done
+}
+
+# Запрашивает подтверждение на выполнение действия, как и confirm,
+# но по умолчанию установлен вариант "N", то есть отказ.
+refuse() {
+    local message=$1
+    while true; do
+        read -p "$(echo -e "${DECOR_BLUE} ${BOLD}${message}?${RESET} [Y/n]: ")" response
+        case "$response" in
+            [Yy]* ) return 0 ;;
+            [Nn]* | "") return 1 ;;
+            * ) echo -e "${DECOR_BLUE} ${FG_YELLOW}Неверный ввод, попробуйте снова.${RESET}" ;;
+        esac
+    done
+}
