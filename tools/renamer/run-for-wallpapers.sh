fd5523b3013b4695cec8aac8461ce954e9b9f15c
cuberbug <gigobug@gmail.com>
2025-10-02 19:11:03 +0300

    Auto: 2025-10-02 16:11:03

--- tools/renamer/run-for-wallpapers.sh
+++ tools/renamer/run-for-wallpapers.sh
@@ -7,31 +7,43 @@ set -o pipefail
 source "$(dirname "${BASH_SOURCE[0]}")/../git-manager/common.sh" || exit 1
 cd "$REPO_ROOT" || exit 1
 
+PATH_TO_RENAMER_DIR="${REPO_ROOT}/tools/renamer"
+PATH_TO_REQUIREMENTS="${REPO_ROOT}/tools/renamer/requirements.txt"
+PATH_TO_VENV_ACTIVATE="${PATH_TO_RENAMER_DIR}/.venv/bin/activate"
+VENV_DIR="$REPO_ROOT/tools/renamer/.venv"
+VENV_PYTHON=""
+
 
 # --- Работа с виртуальным окружением ---
 
-VENV_PYTHON=""
+if [[ -d "$VENV_DIR" ]]; then
+    echo -e "${DECOR_GREEN} Найдено установленное виртуальное окружение Python."
+    VENV_PYTHON="${REPO_ROOT}/tools/renamer/.venv/bin/python"
 
-if [[ -d "$REPO_ROOT/tools/renamer/.venv" ]]; then
-    echo -e "${DECOR_BLUE} Найдено установленное виртуальное окружение Python."
-    echo "Скрипт будет выполнен в виртуальном окружении."
+    if [[ -f "${PATH_TO_REQUIREMENTS}" && -s "${PATH_TO_REQUIREMENTS}" ]]; then
+        install_requirements --activate "${PATH_TO_VENV_ACTIVATE}" \
+                             --python "${VENV_PYTHON}" \
+                             --requirements "${PATH_TO_REQUIREMENTS}"
+    else
+        echo -e "${DECOR_GREEN} Обновление зависимостей не требуется."
+    fi
+
+    echo -e "${DECOR_GREEN} Скрипт будет выполнен в виртуальном окружении."
 else
     echo -e "${DECOR_YELLOW} ${FG_YELLOW}Виртуальное окружение Python не установлено.${RESET}"
     echo "Вы можете установить его автоматически в директорию инструмента или использовать системный интерпретатор."
     echo "Рекомендуется использовать виртуальное окружение, так как это безопасней и позволяет разрешить зависимости."
-    if confirm "Создать виртуальное окружение Python в ./tools/renamer/.venv"; then
 
+    if confirm "Создать виртуальное окружение Python в ./tools/renamer/.venv"; then
         echo -e "${DECOR_BLUE} Создание виртуального окружения..."
-        cd ${REPO_ROOT}/tools/renamer
+        cd ${PATH_TO_RENAMER_DIR}
         python3 -m venv .venv
         VENV_PYTHON="${REPO_ROOT}/tools/renamer/.venv/bin/python"
 
-        if [[ -f "${REPO_ROOT}/tools/renamer/requirements.txt" ]]; then
-            echo -e "${DECOR_BLUE} Обновление пакетного менеджера и установка зависимостей..."
-            source .venv/bin/activate
-            python3 -m pip install --upgrade pip
-            pip install -r requirements.txt
-            deactivate
+        if [[ -f "${PATH_TO_REQUIREMENTS}" && -s "${PATH_TO_REQUIREMENTS}" ]]; then
+            install_requirements --activate "${PATH_TO_VENV_ACTIVATE}" \
+                                 --python "${VENV_PYTHON}" \
+                                 --requirements "${PATH_TO_REQUIREMENTS}"
         else
             echo -e "${DECOR_BLUE} Установка зависимостей не требуется."
         fi
@@ -46,7 +58,7 @@ fi
 # --- Запуск основной логики ---
 
 if confirm "Выполнить переименование всех изображений в директории ./wallpapers"; then
-    cd "${REPO_ROOT}/tools/renamer" || exit 1
+    cd "${PATH_TO_RENAMER_DIR}" || exit 1
 
     PYTHON_CMD="$(choose_python "${VENV_PYTHON}")"
     if ! "$PYTHON_CMD" main.py "${REPO_ROOT}/wallpapers"; then
