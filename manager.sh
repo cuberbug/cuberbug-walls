519ea7e88760e4afea6b57ecdf04d16a5b09e56a
cuberbug <gigobug@gmail.com>
2025-10-17 21:40:12 +0300

    Tools: Release v2.0.1

--- manager.sh
+++ manager.sh
@@ -3,36 +3,77 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Функция для поиска первого доступного терминала из списка
+# Определяет путь до исполняемого файла и переходит в его директорию
+MAIN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$MAIN_PATH" || exit 1
+
+# Настройка сабмодуля
+REPO_TOOLS_PATH="tools"
+
+
+# --- Футкции ---
+
+# Ищет первый доступный терминал из списка и возвращает путь до его бинарника
 find_terminal() {
-    local terminals_to_check=(
-        "konsole"        # Многофункциональный терминал для среды KDE
-        "gnome-terminal" # Стандартный терминал для среды GNOME
-        "gnome-console"  # Легковесный терминал для GNOME, замена gnome-terminal
-        "xfce4-terminal" # Легковесный терминал, идущий в комплекте с XFCE
-        "kitty"          # Быстрый, настраиваемый терминал с поддержкой GPU
-        "alacritty"      # Легковесный, высокопроизводительный терминал с акцентом на простоту
-        "xterm"          # Классический, минималистичный терминал для X Window System
-    )
+  local terminals_to_check=(
+    "konsole"        # Многофункциональный терминал для среды KDE
+    "gnome-terminal" # Стандартный терминал для среды GNOME
+    "gnome-console"  # Легковесный терминал для GNOME, замена gnome-terminal
+    "xfce4-terminal" # Легковесный терминал, идущий в комплекте с XFCE
+    "kitty"          # Быстрый, настраиваемый терминал с поддержкой GPU
+    "alacritty"      # Легковесный, высокопроизводительный терминал с акцентом на простоту
+    "xterm"          # Классический, минималистичный терминал для X Window System
+  )
 
-    for terminal in "${terminals_to_check[@]}"; do
-        if command -v "$terminal" &>/dev/null; then
-            echo "$terminal"
-            return 0
-        fi
-    done
+  for terminal in "${terminals_to_check[@]}"; do
+    if command -v "$terminal" &>/dev/null; then
+      echo "$terminal"
+      return 0
+    fi
+  done
 
-    return 1
+  return 1
 }
+
+# Обновляет сабмодуль до актуального состояния
+#   $1 — путь до сабмодуля (обязательно)
+update_submodule() {
+  local submodule_path=$1
+
+  echo ":: Проверка и обновление сабмодуля '$submodule_path'..."
+
+  if git submodule update --init --remote -- "$submodule_path" &>/dev/null; then
+    echo ":: Сабмодуль успешно обновлен или уже актуален."
+  else
+    git_output=$(git submodule update --init --remote -- "$submodule_path" 2>&1)
+
+    echo "Ошибка: Не удалось обновить сабмодуль '$submodule_path'." >&2
+    echo "Подробности ошибки Git:" >&2
+    echo "$git_output" >&2
+
+    # Можно уронить в критичных случаях:
+    # exit 1 
+  fi
+}
+
+
+# --- Подготовка ---
 
 # Определяет корневую директорию репозитория и терминал для запуска основной логики
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 TARGET_SCRIPT="${REPO_ROOT}/tools/start.sh"
+
+# Обновление сабмодуля repo-tools
+update_submodule "$REPO_TOOLS_PATH"
+
+
+# --- Основная логика запуска ---
+
 TERMINAL=$(find_terminal)
 
 if [[ -z "$TERMINAL" ]]; then
-    echo "Ошибка: Не удалось найти поддерживаемый эмулятор терминала." >&2
-    exit 1
+  echo "Ошибка: Не удалось найти поддерживаемый эмулятор терминала." >&2
+  exit 1
 fi
 
 # Запускает скрипт с основной логикой в новом окне терминала
